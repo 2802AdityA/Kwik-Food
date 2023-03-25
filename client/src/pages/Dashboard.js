f87b6dd07273976ec6725e120e08f55673d1419d
Mahikagarg09 <mahika_garg@yahoo.com>
2023-03-25 18:43:17 +0530

    add to cart

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,14 +2,16 @@ import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Canteen from "./Canteen";
 import Student from "./Student";
+
 const Dashboard = () => {
 	const [email, setEmail] = useState("");
 	const { user } = useOutletContext();
 	useEffect(() => {
-		setEmail(user.email);
+		setEmail(user.email); // eslint-disable-next-line
 	}, []);
-	// adityasingla.2802@gmail.com
-	return email === "muskangarg02270@gmail.com" ? <Student /> : <Canteen />;
+	// const canteenEmail = "muskangarg02270@gmail.com";
+	const canteenEmail = "adityasingla.2802@gmail.com";
+	return email === canteenEmail ? <Student /> : <Canteen />;
 };
 
 export default Dashboard;
